660f7ee858e4a60815d90060988ea40b6d2e7f6b
Jonathan Snyder <jon.jsstudios@gmail.com>
2019-07-03 12:17:35 -0400

    added compression plugin for gzip builds

--- src/components/RouterNavigatorItem.vue
+++ src/components/RouterNavigatorItem.vue
@@ -5,7 +5,7 @@
       v-if="isLeaf"
       :to="routeItem"
       active-class="pointer-events-none active"
-      class="nav__item__title">
+      class="nav__item__title block">
       {{ routeItem.meta.title }}
     </router-link>
 
@@ -53,8 +53,34 @@ export default {
   &__title {
     color: #48eca3;
     font-size: 1rem;
+    text-decoration: none;
+    position: relative;
+    transition: padding 100ms cubic-bezier(0, 0.48, 0.25, 1);
+
+    &:hover {
+      color: saturate(#48eca3, 10);
+      padding-left: 0.5rem;
+    }
 
     &.active {
+      color: #fff;
+      padding-left: 0.5rem;
+
+      &::before {
+        content: '-';
+        display: block;
+        position: absolute;
+        left: 0;
+        top: 0;
+        color: #fff;
+
+      }
+    }
+
+    &.active,
+    &:hover,
+    &:focus {
+      outline: none;
       color: #fff;
     }
   }
@@ -63,9 +89,12 @@ export default {
     font-size: 0.7em;
 
     .nav__item__title {
-      color: rgba(247, 247, 247, 0.34);
+      color: rgba(247, 247, 247, 0.5);
 
-      &.active {
+      &.active,
+      &:hover,
+      &:focus {
+        outline: none;
         color: #fff;
       }
     }
